2df036d0421db558e77fa01c4cc94c007d3572aa
RomanMainAcc <romanborisuk0@gmail.com>
2023-08-26 18:08:45 +0300

    before hierarchy refactor, redirect_router.py

---
+++ ShortURL_app/main_routers/redirect_router.py
@@ -0,0 +1,23 @@
+from fastapi import APIRouter, Depends, HTTPException
+from fastapi.responses import RedirectResponse
+from sqlalchemy.orm import Session
+
+import models
+from database import get_db
+
+router = APIRouter(
+    prefix="",
+    tags=["Redirect"]
+)
+
+
+@router.get("/{short_link}")
+def redirect(short_link: str, db: Session = Depends(get_db)):
+    record = db.query(models.LinkTable).filter_by(short_link=short_link).first()
+
+    if record:
+        record.total_clicks += 1
+        db.commit()
+        return RedirectResponse(url=record.link)
+    else:
+        raise HTTPException(status_code=404, detail="Short link not found")
